1eab2f85b2b502087760ea89009e919e427b7ca0
sonakshi10 <45477657+sonakshi10@users.noreply.github.com>
2020-11-29 23:02:18 +0530

    Add files via upload

---
+++ Week_2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace q3
+{
+    public delegate void TrafficDel();
+    class TrafficSignal
+    {
+       public void Yellow()
+        {
+            Console.WriteLine("Yellow");
+        }
+        public void Green()
+        {
+            Console.WriteLine("Green");
+        }
+        public void Red()
+        {
+            Console.WriteLine("Red");
+        }
+        static void Main(string[] args)
+        {
+            TrafficSignal ts = new TrafficSignal();
+            TrafficDel deltraffic = new TrafficDel(ts.Yellow);
+            deltraffic();
+            deltraffic += ts.Green;
+            deltraffic();
+            deltraffic += ts.Red;
+            deltraffic();
+        }
+    }
+
+    
+}
