bd8982e1eb53578c56911ce4d7a0fa8284a7184d
sonakshi10 <45477657+sonakshi10@users.noreply.github.com>
2020-11-29 23:01:05 +0530

    Add files via upload

---
+++ Week_2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace q2
+{
+   class Item
+    {
+        string name;
+        public static double cost;
+        public string Name { get; set; }
+
+        public static double CalcGST()
+        {
+            return 0.08 * cost;
+        }
+    }
+    class Test
+    {
+        static void Main(string[] args)
+        {
+            Item item = new Item();
+            item.Name = "item1";
+            Item.cost = 24;
+            double gst = Item.CalcGST();
+            Console.WriteLine("GST: {0}", gst);
+        }
+    }
+}
