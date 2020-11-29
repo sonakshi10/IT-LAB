a4fb5f4756e08f503e429de25600a0a4f2f261c8
sonakshi10 <45477657+sonakshi10@users.noreply.github.com>
2020-11-29 22:59:35 +0530

    Add files via upload

---
+++ Week_2/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace q1
+{
+    public delegate void PriceChangedEventHandler();
+    class Item
+    {
+        string name;
+        double price;
+        public string Name
+        {
+            get; set;
+        }
+        
+        public event PriceChangedEventHandler PriceChanged;
+        public double Price
+        {
+            get
+            {
+                return price;
+            }
+            set
+            {
+                price = value;
+                if(PriceChanged != null)
+                {
+                    PriceChanged();
+                }
+            }
+        }
+       
+    }
+    class Subscriber
+    {
+        
+        static void Main(string[] args)
+        {
+            Item item = new Item();
+            item.Price = 24;
+            item.PriceChanged += ChangeDetected;
+            Console.WriteLine("Enter price: ");
+            double price;
+            double.TryParse(Console.ReadLine(), out price);
+            item.Price = price;
+        }
+        public static void ChangeDetected()
+        {
+            Console.WriteLine("Price changed!");
+        }
+    }
+}
