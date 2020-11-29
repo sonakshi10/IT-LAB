6396e2f8a9764dd6c9e8f4d3d722784f48f94d26
sonakshi10 <45477657+sonakshi10@users.noreply.github.com>
2020-11-29 22:50:38 +0530

    Add files via upload

---
+++ Week_1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace q1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter two numbers: ");
+            double num1, num2;
+            string n1 = Console.ReadLine();
+            string n2 = Console.ReadLine();
+            double.TryParse(n1, out num1);
+            double.TryParse(n2, out num2);
+            Console.WriteLine("The result of : \n Addition: {0} + {1} = {2} \n" +
+                " Subtraction: {0} - {1} = {3} \n Division: {0} / {1} = {4} \n Multiplication: " +
+                "{0} * {1} = {5}", num1, num2, num1+num2, num1-num2, num1/num2, num1*num2);
+            Console.ReadLine();
+
+        }
+    }
+}
